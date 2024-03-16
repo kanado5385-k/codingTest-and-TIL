71092fa3f8ee032b1484a39909d1ffa93e117b30
이윤빈 <81556800+kanado5385-k@users.noreply.github.com>
2024-03-16 15:08:24 +0900

    Update a15624.java

---
+++ Silver/a15624.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        long[] DP = new long[1000001];
+        DP[1] = 1;
+        for(int i = 2; i < n+1; i++){
+            DP[i] = DP[i-1] + DP[i-2];
+        }
+        
+        long sum = DP[n] % 1000000007;
+
+        System.out.print(sum);
+    }
+}
