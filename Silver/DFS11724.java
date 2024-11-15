f23e54379c590f726ab3500f890271921f679138
이윤빈 <kanado20000@naver.com>
2024-11-15 19:11:03 +0900

    feat: 백준 실버2 11724 DFS 문제 해결

---
+++ Silver/DFS11724.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DFS11724 {
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int result = 0;
+    static boolean[] visited;
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < n + 1; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        for (int i = 1; i <= n; i++){
+            if (visited[i] == false){
+                result++;
+                dfs(i);
+            }
+        }
+
+        System.out.println(result);
+        sc.close();
+    }
+
+    static void dfs(int node){
+        visited[node] = true;
+
+        for(int childNode : graph.get(node)){
+            if (visited[childNode] == false){
+                dfs(childNode);
+            }
+        }
+    }
+}
